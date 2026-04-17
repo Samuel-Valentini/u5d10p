bc8850f317c36bfbd2ef68f9863138c0ed359aa3
Samuel-Valentini <samuelvitalia@gmail.com>
2026-04-17 11:12:58 +0200

    Initial commit

---
+++ src/main/java/samuelvalentini/u5d10p/U5d10pApplication.java
@@ -0,0 +1,13 @@
+package samuelvalentini.u5d10p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5d10pApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(U5d10pApplication.class, args);
+    }
+
+}
